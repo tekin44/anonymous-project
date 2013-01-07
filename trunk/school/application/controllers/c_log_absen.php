7178d5143536e739874e0b647c13abe21c9196db
Adiguna.Ginanjar@gmail.com <Adiguna.Ginanjar@gmail.com@c2a0df1a-8be5-0e1c-b92f-c6c7a8dd24e9>
2013-01-07 15:18:38 +0000

    Punten cep coba lagi, pang buatin gimana caranya supaya di navigasi nya ada menu untuk log_absen ini
    punten eta urang nggak tau soalnya ada hubungannya dengan model/database
    kalo misalkan riweuh mah kasih tau urang aja gmn cara bikin nya
    
    yoo bro, sorry ngahesekeun wae yeuh

--- trunk/school/application/controllers/c_log_absen.php
+++ trunk/school/application/controllers/c_log_absen.php
@@ -8,12 +8,13 @@
 //exit ('No direct script access allowed');
 
 class c_log_absen extends CI_Controller{
+    
     function __construct()
     {
         parent::__construct();
         $this->load->model('m_menu');
-	//$this->client_logon = $this->session->userdata('login');
-	//$this->data['menus'] = $this->m_menu->getAll($this->client_logon[0]->admin_username);                
+	$this->client_logon = $this->session->userdata('login');
+	$this->data['menus'] = $this->m_menu->getAll($this->client_logon[0]->admin_username);    
     }
     
     public function index()
@@ -22,14 +23,14 @@ class c_log_absen extends CI_Controller{
         ** di index, belum nampilin/ fetching data apa apa, cuma buat nampilin view doang
         */        
         
-        //$this->data['title'] = "Log Absen Server & Finger Print";
-	//$this->data['action'] = "/school/c_log_absen/calculate_total";
-	//$this->load->view('v_header', $this->data);
-	//$this->load->view('v_log_absen', $this->data);
-        //$this->load->view('v_footer', $this->data);        
+        $this->data['jum_server'] = 0;
+        $this->data['jum_fingerprint'] = 0;
+        $this->data['jum_selisih'] = 0;
+        $this->data['action'] = "/school/c_log_absen/calculatetotal";
+	$this->load->view('v_log_absen', $this->data);                
     }
     
-    public function calculate_total()
+    public function calculatetotal()
     {
         /*
          * calc_total di panggil kalo submit, cuma submit tanggal aja
@@ -43,24 +44,27 @@ class c_log_absen extends CI_Controller{
          *          + record dari log fingerprint
          *          + record log yang ada di fp tapi nggak di server
          */
-        
-        $date = $_REQUEST['date'];
         $this->load->model('m_log_absen');
+        
+        $day = $_REQUEST['d'];
+        $month = $_REQUEST['m'];
+        $year = $_REQUEST['y'];
+        
+        $date =  $year.'-'.$month.'-'.$day;                     
         
         $jum_server = $this->m_log_absen->get_server($date);
         $jum_fp = $this->m_log_absen->get_fingerprint($date);
-        $jum_selisih = $this->m_log_absen->get_exluded($date);
+        $jum_selisih = $this->m_log_absen->get_excluded($date);
         
-        $rec_server = $this->m_log_absen->get_rec_absen($date);
-        $rec_fp = $this->m_log_absen->get_rec_fingerprint($date);
-        $rec_selisih = $this->m_log_absen->get_rec_excluded($date);
+//        $rec_server = $this->m_log_absen->get_rec_server($date);
+//        $rec_fp = $this->m_log_absen->get_rec_fingerprint($date);
+//        $rec_selisih = $this->m_log_absen->get_rec_excluded($date);                
         
-        
-//	$this->data['title'] = "Log Absen Server & Finger Print";
-//	$this->data['action'] = "/school/c_log_absen/calculate_total";
-//	$this->load->view('v_header', $this->data);
-//	$this->load->view('v_log_absen', $this->data);
-//        $this->load->view('v_footer', $this->data);
+        $this->data['jum_server'] = $jum_server[0]->count;
+        $this->data['jum_fingerprint'] = $jum_fp[0]->count;
+        $this->data['jum_selisih'] = $jum_selisih[0]->count;
+        $this->data['action'] = "/school/c_log_absen/calculatetotal";
+	$this->load->view('v_log_absen', $this->data);    
     }
     
 }
